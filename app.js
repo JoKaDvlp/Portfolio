ca6e10d96fefa5c92da179deb90715f614f940b3
JoKaDvlp <jonathan.karmann@gmail.com>
2024-03-15 16:23:47 +0100

    Reset
    Remplacement du pop-up par un accordéon

--- app.js
+++ app.js
@@ -1,61 +1,10 @@
-let about = document.getElementById("about")
-let contact = document.getElementById("contact")
-let projets = document.getElementById("projets")
-let echaps = document.querySelectorAll(".modal img")
-let modal = document.querySelector(".modal")
+const cards = document.querySelectorAll(".card")
 
-let aboutContent = document.querySelector(".about-content")
-let contactContent = document.querySelector(".contact-content")
-let projetsContent = document.querySelector(".projets-content")
-
-about.addEventListener("click",()=>{
-    let divActive = document.querySelector(".open")
-    if (divActive) {
-        divActive.classList.remove("open")
-        divActive.classList.add("none")
-    }
-    let titreActive = document.querySelector(".blue")
-    if (titreActive) {
-        titreActive.classList.remove("blue")
-    }
-    aboutContent.classList.add("open")
-    aboutContent.classList.remove("none")
-    about.classList.add("blue")
-})
-contact.addEventListener("click",()=>{
-    let divActive = document.querySelector(".open")
-    if (divActive) {
-        divActive.classList.remove("open")
-        divActive.classList.add("none")
-    }
-    let titreActive = document.querySelector(".blue")
-    if (titreActive) {
-        titreActive.classList.remove("blue")
-    }
-    contactContent.classList.add("open")
-    contactContent.classList.remove("none")
-    contact.classList.add("blue")
-})
-projets.addEventListener("click",()=>{
-    let divActive = document.querySelector(".open")
-    if (divActive) {
-        divActive.classList.remove("open") 
-        divActive.classList.add("none")       
-    }
-    let titreActive = document.querySelector(".blue")
-    if (titreActive) {
-        titreActive.classList.remove("blue")
-    }
-    projetsContent.classList.add("open")
-    projetsContent.classList.remove("none")
-    projets.classList.add("blue")
-})
-echaps.forEach(echap=>{
-    echap.addEventListener("click",()=>{
-        let titreActive = document.querySelector(".blue")
-        let divActive = document.querySelector(".open")
-        divActive.classList.remove("open")
-        divActive.classList.add("none")
-        titreActive.classList.remove("blue")
+cards.forEach(card => {
+    card.addEventListener("click",()=>{
+        // let ouvert = document.querySelector(".ouvert")
+        let ouvert = document.querySelector(".ouvert")
+        ouvert.classList.remove("ouvert")
+        card.classList.add("ouvert")
     })
-})
+});
