4318fb6d741fc6658d6c5f1eb9c10d781b5057da
JoKaDvlp <jonathan.karmann@gmail.com>
2024-03-25 11:25:33 +0100

    Section projets
    Ajout des projets en CSS grid

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const cards = document.querySelectorAll(".card")
 
 cards.forEach(card => {
     card.addEventListener("click",()=>{
-        // let ouvert = document.querySelector(".ouvert")
         let ouvert = document.querySelector(".ouvert")
         ouvert.classList.remove("ouvert")
         card.classList.add("ouvert")
     })
 });
+
+const swiper = new Swiper(".swiper", {
+    effect: "slide",
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
